7565d40a66f27b050d5e9bff61203365d60ceff3
Kousttav Biswas <kousttavb@gmail.com>
2026-09-19 11:23:55 +0530

    Updated

--- backend/server_backend/server.js
+++ backend/server_backend/server.js
@@ -1,7 +1,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-import { collectAndForward, getLatestReading } from "./controllers/realtimeController.js";
+import { collectAndForward, getLatestReading } from "./controllers/Realtimecontroller.js";
 dotenv.config();
 
 const app = express();
